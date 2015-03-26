f5a80618a2908c83efe738fa6461e6dce6074d9a
Vial-Grelier Aymeric <Aym@macbook-air-de-vial-grelier.home>
2015-03-26 23:43:45 +0100

    commentary modification
    added some comment

--- src/exception/LispException.java
+++ src/exception/LispException.java
@@ -4,7 +4,8 @@ public class LispException extends Exception {
 	
 	/**
 	 * Don't really know what it is used for.
-	 * IDE would shout out if not there.
+	 * IDE would shout out if not there. 
+	 * To modify
 	 */
 	private static final long serialVersionUID = 1L;
 
