2ad890d1fdbde57dbd08d1d7371698d263f5c5e5
Aymeric Vial-Grelier <aymvialgrelier@yahoo.fr>
2015-03-26 23:33:17 +0100

    modification on exception
    Added commentary

--- src/exception/LispException.java
+++ src/exception/LispException.java
@@ -1,12 +1,17 @@
 package exception;
 
 public class LispException extends Exception {
-
+	
 	/**
-	 * 
+	 * Don't really know what it is used for.
+	 * IDE would shout out if not there.
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor with message
+	 * @param message
+	 */
 	public LispException(String message) {
         super(message);
     }
