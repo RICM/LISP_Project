dc1778854232eec0e8d2ee581a5d07bba22c7d3d
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2015-04-06 23:40:51 +0200

    fixed main with parser error

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,19 @@
 package main;
 
 import reader.*;
+import exception.ParseException;;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Reader r = new Reader();
-
 		
-		eval(r.read());
+			try {
+				reader2.main(args);
+			} catch (ParseException e) {
+				System.out.println(e.getMessage());
+			}
+		
 		
 	}
 
