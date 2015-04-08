89686f8aedce7b492940f212de31ffb78e1207de
Aymeric VIAL-GRELIER <aymvial@gmail.com>
2015-04-08 20:47:33 +0200

    implement getNumberOfParam
    documentation

--- src/lisp/Fexpr.java
+++ src/lisp/Fexpr.java
@@ -2,15 +2,34 @@ package lisp;
 
 public class Fexpr extends AbstrFunction {
 
+	/**
+	 * Private attributes
+	 */
+	private int numberOfParam;
+	
+	/**
+	 * Constructor with number of parameters
+	 * @param numberOfParam The number of parameters
+	 */
+	public Fexpr(int numberOfParam){
+		this.numberOfParam = numberOfParam;
+	}
+	
+	/**
+	 * Function used to get the number of parameters
+	 * @return int The number of parameters of the Fexpr function
+	 */
 	@Override
 	public int getNumberOfParam() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.numberOfParam;
 	}
 
+	/**
+	 * Function used to apply an _Sexpr to the Fexpr function
+	 * @return _Sexpr The _Sexpr resulting from application
+	 */
 	@Override
 	public _Sexpr apply(_Sexpr param) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
--- src/lisp/function/subr/Cdr.java
+++ src/lisp/function/subr/Cdr.java
@@ -6,20 +6,47 @@ import lisp._Sexpr;
 
 public class Cdr extends AbstrSubr{
 
+	/**
+	 * Private Attributes
+	 */
+	private int numberOfParam;
+	
+	/**
+	 * Public Attributes (strange)
+	 */
 	public _Sexpr cdr;
 
+	/**
+	 * Constructor with number of parameters
+	 * @param numberOfParam The number of parameters
+	 */
+	public Cdr(int numberOfParam){
+		this.numberOfParam = numberOfParam;
+	}
+	
+	/**
+	 * Function used to evaluate a Cdr
+	 * @return _Sexpr then _Sexpr resulting from evaluation
+	 */
 	@Override
 	public _Sexpr eval() throws LispException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Function used to get the number of parameters
+	 * @return int The number of parameters of the Cdr function
+	 */
 	@Override
 	public int getNumberOfParam() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.numberOfParam;
 	}
 
+	/**
+	 * Function used to apply an _Sexpr to the Cdr function
+	 * @return _Sexpr The _Sexpr resulting from application
+	 */
 	@Override
 	public _Sexpr apply(_Sexpr param) {
 		// TODO Auto-generated method stub
