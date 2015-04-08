fbbb8b75bf70b5a9a8c8e91ae63e9053d2760053
mnogueron <matthieu.nogueron@live.fr>
2015-04-08 17:56:28 +0200

    add exe function into AbstrFsubr to apply a parameter lisp to a function

--- src/lisp/AbstrFsubr.java
+++ src/lisp/AbstrFsubr.java
@@ -1,6 +1,17 @@
 package lisp;
 
+import exception.LispException;
+
 
 public abstract class AbstrFsubr extends AbstrPrimitive{
-
+	/**
+	 * Apply the list of parameters to the function.
+	 * @param fun The function to use.
+	 * @param param The list of parameters to use inside the function.
+	 * @return The application of the parameter list to the function.
+	 * @throws LispException
+	 */
+	public _Sexpr exec(_Function fun, _Sexpr param) throws LispException {
+		return fun.apply(param);
+	}
 }
