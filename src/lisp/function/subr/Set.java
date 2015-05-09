a9f301c037d37eb3889bec5119f66e15a1d0d37f
mnogueron <matthieu.nogueron@live.fr>
2015-05-09 18:51:08 +0200

    add set function

---
+++ src/lisp/function/subr/Set.java
@@ -0,0 +1,51 @@
+package lisp.function.subr;
+
+import context.Contexts;
+import lisp.AbstrSubr;
+import lisp.Nil;
+import lisp.Scons;
+import lisp.Symbol;
+import lisp._Sexpr;
+import exception.LispException;
+
+public class Set extends AbstrSubr{
+
+	/**
+	 * Private Attributes
+	 */
+	private int numberOfParam = 2;
+
+	/**
+	 * Function used to evaluate a Car
+	 * @return _Sexpr then _Sexpr resulting from evaluation
+	 */
+	@Override
+	public _Sexpr eval() throws LispException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Function used to get the number of parameters
+	 * @return int The number of parameters of the Car function
+	 */
+	@Override
+	public int getNumberOfParam() {
+		return this.numberOfParam;
+	}
+
+	/**
+	 * Function used to apply an _Sexpr to the Car function
+	 * @return _Sexpr The _Sexpr resulting from application
+	 * @throws LispException 
+	 */
+	@Override
+	public _Sexpr apply(_Sexpr param) throws LispException{
+		if(!(param.getCar() instanceof Symbol))
+			throw new LispException("Error : trying to apply SET to another thing than a Symbol");
+		else{
+			Contexts.changeSexprInContexts((Symbol)param.getCar(), param.getCdr());
+			return param.getCdr();
+		}
+	}
+}
