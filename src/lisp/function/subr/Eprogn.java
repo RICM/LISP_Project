d89d7ee3e9bce896f1c892d9b9e2b7ba3832ef74
mnogueron <matthieu.nogueron@live.fr>
2015-05-10 17:37:05 +0200

    add eprogn function

---
+++ src/lisp/function/subr/Eprogn.java
@@ -0,0 +1,50 @@
+package lisp.function.subr;
+
+import lisp.AbstrSubr;
+import lisp.Nil;
+import lisp._Sexpr;
+import exception.LispException;
+
+public class Eprogn extends AbstrSubr{
+
+	/**
+	 * Private Attributes
+	 */
+	private int numberOfParam = 1;
+
+	/**
+	 * Function used to evaluate a Car
+	 * @return _Sexpr then _Sexpr resulting from evaluation
+	 */
+	@Override
+	public _Sexpr eval() throws LispException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Function used to get the number of parameters
+	 * @return int The number of parameters of the Car function
+	 */
+	@Override
+	public int getNumberOfParam() {
+		return this.numberOfParam;
+	}
+
+	/**
+	 * Function used to apply an _Sexpr to the Car function
+	 * @return _Sexpr The _Sexpr resulting from application
+	 * @throws LispException 
+	 */
+	@Override
+	public _Sexpr apply(_Sexpr param) throws LispException{
+		_Sexpr tmp = param;
+		_Sexpr out = null;
+		while(tmp != Nil.nil){
+			out = tmp.getCar().eval();
+			tmp = tmp.getCdr();
+		}
+		return out;
+	}
+}
+
